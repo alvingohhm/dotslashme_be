25ac8f1dd06b5ea79f2689ad553ccdc47331bf40
alvingohhm <alvgoh@yahoo.com>
2022-02-08 08:13:34 +0800

    fixed updateProfile and getProfile bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,22 +13,52 @@ const userController = {
           .json(jsonMessages(401, "no", "User not found", []));
       }
 
+      //update user
+      user.set({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+      });
+      await user.save().catch((error) => {
+        console.log(error);
+        return res
+          .status(400)
+          .json(jsonMessages(400, "no", "Unable to update profile", []));
+      });
+
+      //update profile section
       let profile = await user.getProfile();
+
       if (!profile) {
-        profile = await user.createProfile(req.body);
-        const data = [profile.toJSON()];
+        profile = await user.createProfile(req.body.Profile);
+
+        const finalResult = {
+          first_name: user.first_name,
+          last_name: user.last_name,
+        };
+        const { id, createdAt, updatedAt, user_id, ...updatedProfile } =
+          profile.toJSON();
+        finalResult.Profile = updatedProfile;
+
         return res
           .status(201)
-          .json(jsonMessages(201, "yes", "Profile created", data));
+          .json(jsonMessages(201, "yes", "Profile created", [finalResult]));
       } else {
         //update instead
-        profile.set(req.body);
+        profile.set(req.body.Profile);
         await profile
           .save()
           .then((data) => {
+            const finalResult = {
+              first_name: user.first_name,
+              last_name: user.last_name,
+            };
+            const { id, createdAt, updatedAt, user_id, ...updatedProfile } =
+              data.toJSON();
+            finalResult.Profile = updatedProfile;
+
             return res
               .status(201)
-              .json(jsonMessages(201, "yes", "Profile updated", data));
+              .json(jsonMessages(201, "yes", "Profile updated", [finalResult]));
           })
           .catch((error) => {
             console.log(error);
